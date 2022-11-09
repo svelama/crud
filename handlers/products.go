1d3897372d38f9ad722c45f38c614d89f4e25912
Sankar Velama <svelama@outlook.com>
2022-11-09 20:49:30 +0000

    added gorilla mux

--- handlers/products.go
+++ handlers/products.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/svelama/go/http/data"
 )
 
@@ -17,44 +18,67 @@ func NewProducts(l *log.Logger) *Products{
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request){
+func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request){
 
-	if r.Method == http.MethodGet {
-		getProducts(w, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		addProducts(w, r)
-		return
-	}
-
-	// Return 
-	w.WriteHeader(http.StatusMethodNotAllowed)
-}
-
-func getProducts(w http.ResponseWriter, r *http.Request){
-
+	p.l.Println("Get method called")
 	lp := data.GetProducts()
 	if err := lp.ToJson(w); err != nil {
 		http.Error(w, "failed to fetch products", http.StatusInternalServerError)
 	}
 }
 
-func addProducts(w http.ResponseWriter, r *http.Request){
+func (p *Products) AddProducts(w http.ResponseWriter, r *http.Request){
 
+	p.l.Println("Post method called")
 	raw, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "failed to add products", http.StatusBadRequest)
 		return
 	}
 
-    p := &data.Product{}
-	err = json.Unmarshal(raw, p)
+    product := &data.Product{}
+	err = json.Unmarshal(raw, product)
 	if err != nil {
 		http.Error(w, "failed to unmarshal json", http.StatusBadRequest)
 		return
 	}
 	
-	data.AddProducts(p)
+	data.AddProducts(product)
+}
+
+func (p *Products) UpdateProducts(w http.ResponseWriter, r *http.Request){
+	
+	vars := mux.Vars(r)
+	id := vars["id"]
+	
+	p.l.Println("Put method called, id: ", id)
+
+	// Get Products object from Json
+	product := &data.Product{}
+	err := product.FromJson(r.Body)
+	if err != nil {
+		http.Error(w, "failed to unmarshal json", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProducts(product, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func (p *Products) DeleteProducts(w http.ResponseWriter, r *http.Request){
+	p.l.Println("Delete method called")
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if !data.RemoveProducts(id) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Product does not exist"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Product successfully deleted"))
 }
